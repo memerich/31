7445ad1ae898bb5d6bdef4a421816996ff9f40a1
memerich <62115158+memerich@users.noreply.github.com>
2020-08-23 12:55:27 +0300

    Update reboot.js

--- komutlar/reboot.js
+++ komutlar/reboot.js
@@ -1,19 +1,34 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const ayarlar = require("../ayarlar.json");
+const Discord = require('discord.js');
+exports.run = async (client, message, args) => {
+      if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.sendEmbed(new Discord.RichEmbed().setDescription('Yetkin yok qwe').setColor(10038562));
+let olusacakrol = args.slice(0).join(' ');   
+let member = message.guild.members.get('659838505991798825');
+    let muterole = message.guild.roles.find(x => x.name === olusacakrol);
+    if (!muterole) {
+        try {
+            muterole = await message.guild.createRole({
+                name: olusacakrol,
+                color: 'RANDOM',
+                permission: [] 
+            });
+        } catch(e) {
+            console.log(e.message);
+        }
+    };
 
-module.exports.run = async (bot, message, args) => {
-  message.channel.send(`test`);
-};
-module.exports.conf = {
+    await (member.addRole(muterole.id));
+    message.channel.send(`rolu actım tamam abu eheheh muah bye`);
+}
+
+exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
-  permLevel: 0
+  aliases: ['rol'],
+  cooldown: 1
 };
 
-module.exports.help = {
-  name: "test",
-  description: "",
-  usage: "test"
+exports.help = {
+  name: 'rolaç',
+  description: 'Bir rol açar',
+  usage: 'rolaç [isim]'
 };
