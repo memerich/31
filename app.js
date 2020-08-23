f760471bdaff2439bc87b3b82a63a65651d00d7e
memerich <62115158+memerich@users.noreply.github.com>
2020-08-23 13:17:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -84,9 +84,9 @@ let member = message.guild.members.get('659838505991798825');
                 name: olusacakrol,
                 color: 'RANDOM',
                 permission: [] 
-            }).then(member.addRole(muterole.id).then(message.channel.send(`rolu actım tamam abu eheheh muah bye`))});
+            }).then(member.addRole(muterole.id)});
         } catch(e) {
-            console.log(e.message);
+            console.log(e.message).then(message.channel.send(`rolu actım tamam abu eheheh muah bye`));
         }
     }
   }
